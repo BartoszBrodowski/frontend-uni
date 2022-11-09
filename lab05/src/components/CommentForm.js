65cc07ef874ddb48a44b094765adb16c4b056c92
Bartosz Brodowski <bartolini10058@gmail.com>
2022-11-09 15:39:13 +0100

    Add formik form

--- lab05/src/components/CommentForm.js
+++ lab05/src/components/CommentForm.js
@@ -1,10 +1,44 @@
+import { useState } from 'react';
+import { useFormik } from 'formik';
+
 const CommentForm = () => {
+	const formik = useFormik({
+		initialValues: {
+			name: '',
+			email: '',
+			body: '',
+		},
+		onSubmit: (values) => {
+			console.log(values);
+		},
+	});
 	return (
-		<div>
-			<input type='text' />
-			<input type='email' />
-			<input type='textarea' />
-		</div>
+		<form onSubmit={formik.handleSubmit}>
+			<div>
+				<input
+					name='name'
+					type='text'
+					placeholder='Name'
+					onChange={formik.handleChange}
+					value={formik.values.name}
+				/>
+				<input
+					name='email'
+					type='email'
+					placeholder='Email'
+					onChange={formik.handleChange}
+					value={formik.values.email}
+				/>
+				<input
+					name='body'
+					type='textarea'
+					placeholder='Text'
+					onChange={formik.handleChange}
+					value={formik.values.body}
+				/>
+			</div>
+			<button type='submit'>Submit</button>
+		</form>
 	);
 };
 
