e1bad84f3f41ca90d687931bfcf2de88263c2c2c
Bartosz Brodowski <bartolini10058@gmail.com>
2022-11-09 15:19:11 +0100

    Setup project, add basic components

---
+++ lab05/src/App.js
@@ -0,0 +1,11 @@
+import CommentForm from './components/CommentForm';
+
+function App() {
+	return (
+		<div className='App'>
+			<CommentForm />
+		</div>
+	);
+}
+
+export default App;
---
+++ lab05/src/components/CommentForm.js
@@ -0,0 +1,11 @@
+const CommentForm = () => {
+	return (
+		<div>
+			<input type='text' />
+			<input type='email' />
+			<input type='textarea' />
+		</div>
+	);
+};
+
+export default CommentForm;
